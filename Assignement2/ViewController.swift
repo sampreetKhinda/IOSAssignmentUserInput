a66fb8ab88bac6491b4b2bca4bc7eeff46d2f4f9
Sampreet <sampreetsingh0903@gmail.com>
2024-01-29 15:38:59 -0500

    Initial Commit

--- Assignement2/ViewController.swift
+++ Assignement2/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstName: UITextField!
+    
+    @IBOutlet weak var lastName: UITextField!
+    
+    @IBOutlet weak var countryName: UITextField!
+    
+    @IBOutlet weak var age: UITextField!
+    
+    @IBOutlet weak var outputTV: UITextView!
+    
+    @IBOutlet weak var alertLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func clearBtn(_ sender: Any) {
+        firstName.text = ""
+        lastName.text = ""
+        countryName.text = ""
+        age.text = ""
+        outputTV.text = ""
+        alertLabel.text = ""
+    }
+    @IBAction func addDetailsBtn(_ sender: Any) {
+        var finalStr :String? = ""
+        finalStr = " Full Name : \(firstName.text!)  \(lastName.text!) \n Country : \(countryName.text!) \n Age : \(age.text!)"
+        outputTV.text = finalStr!
+        alertLabel.text = ""
+    }
+    
+    @IBAction func submitBtn(_ sender: Any) {
+  
+        if(firstName.text == "" ||
+           lastName.text == "" ||
+           countryName.text == "" ||
+           age.text == ""){
+            alertLabel.text = "Complete the missing Info!"
+        } else {
+            alertLabel.text = "Successfully submitted!"
+        }
+    }
+    
+    
 }
 
